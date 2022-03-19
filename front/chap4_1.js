e876e650084d59fc526bdf22397c6fd33f332809
JaeHyun Park <jhpark4884@gmail.com>
2022-03-20 01:20:20 +0900

    a

---
+++ front/chap4_1.js
@@ -0,0 +1,14 @@
+let a = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be.";
+
+for (let i = 0; i < a.length; i++) {
+    if (a[i] == 'a') {
+        console.log(i);
+    } else if (a[i] == 'e')
+        console.log(i);
+    else if (a[i] == 'i')
+        console.log(i);
+    else if (a[i] == 'o')
+        console.log(i);
+    else if (a[i] == 'u')
+        console.log(i);
+}
